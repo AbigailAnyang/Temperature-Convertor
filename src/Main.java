50d15b603e1c7776cd331a3286588110a4ae4763
Abigail Anyang <abigailanyang24@gmail.com>
2024-05-22 10:33:08 -0700

    First commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 
 public class Main {
-    static double tempConv(int t) {
-        return(t * 9/5) + 32;
+    static float tempConv(int t) {
+        return (t * 9/5) + 32;
     }
 
     public static void main(String[] args) {
-        int t = 60;
+        int t = 60; //Sample Test.
         System.out.println(tempConv(t));
         }
     }
