cd19dd0754c17067e3f09141c9942bbc3ccf7747
Abigail Anyang <abigailanyang24@gmail.com>
2024-05-22 09:33:20 -0700

    First commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    static double tempConv(int t) {
+        return(t * 9/5) + 32;
+    }
+
+    public static void main(String[] args) {
+        int t = 60;
+        System.out.println(tempConv(t));
+        }
+    }
